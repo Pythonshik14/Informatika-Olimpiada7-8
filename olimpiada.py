566c47d322160dd73587b613c0b6ac41d314244b
Elan Bes <81530169+Pythonshik14@users.noreply.github.com>
2021-08-23 18:12:25 +0300

    Add files via upload

---
+++ olimpiada.py
@@ -0,0 +1,34 @@
+# #task1
+# import sys
+
+# # def palindrom(n):
+# #     m = sys.maxsize
+# #     for k in range(n+1, m):
+# #         if str(k) == str(k)[::-1]:
+# #             return k
+
+# # print(palindrom(54321))
+
+
+# #task2
+# inp_digit = list(map(int, input(": ")))
+# print(inp_digit)
+# print(sum(inp_digit))
+
+#task3
+# inps = list(map(int, input("Введите кол-во n-клетки, m-кролики : ").split()))
+
+# if inps[0] > inps[1]:
+#     print(inps[0] - inps[1])
+
+# else:
+#     print(inps[1] - inps[0] + 1)
+
+#task4
+print("Ответ: ", eval(input("Введите:")))
+
+
+
+
+
+
